0b35f48d83f7a18cc4171b4f5563fcb3ae1896f7
Nikita Martynyuk <mnekita@yandex.ru>
2023-04-30 13:56:06 +0500

    first commit

--- Homework2_13/Program.cs
+++ Homework2_13/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//  что третьей цифры нет.Через строку решать нельзя.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Получаем третью цифру числа
+        int thirdDigit = (number / 1) % 10;
+        
+        if (thirdDigit == -1)
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+        else
+        {
+            
+            Console.WriteLine($"Третья цифра числа {number} равна {thirdDigit}");
+        }
+        
