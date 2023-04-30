4620ec6366c5820d33c4c3d1b9cb472ec56fc7e0
Nikita Martynyuk <mnekita@yandex.ru>
2023-04-30 14:00:58 +0500

    Закинул фото с ДЗ

--- Homework2_Neob_1/Program.cs
+++ Homework2_Neob_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// на входе целое или вещественное число,
+//  надо удалить вторую цифру слева этого числа
+
+// 145 -> 15
+// 1 -> нет
+// 567,123 -> 57,123
+
+Console.Write("Введите число: ");
+        string input = Console.ReadLine();
+
+        if (input.Length < 3) {
+            Console.WriteLine("нет");
+        } else {
+            string output = input.Remove(1, 1);
+            Console.WriteLine(output);
+        }
