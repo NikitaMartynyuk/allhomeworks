084b351508b792c7049ec17866ac7b0e5b2174c9
Nikita Martynyuk <mnekita@yandex.ru>
2023-05-24 14:48:23 +0500

    Сделал задачи 25 и 27

--- Example17/Program.cs
+++ Example17/Program.cs
@@ -1,23 +1,84 @@
 ﻿// Задача 17 Напишите программу которая принимает на вход координаты точки (X и Y),
 // причем х не= 0 и у не=0 и выдает номер четверти плоскости в которой находится эта точка  
 
-Console.WriteLine("Введите координату X");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату Y");
-int y = Convert.ToInt32(Console.ReadLine());
+
+
+// void CheckKoord(int x, int y)
+// // {
+// //     if (x > 0 && y > 0)
+// //         System.Console.WriteLine("Четверть № 1");
+// //     else if (x > 0 && y < 0)
+// //         System.Console.WriteLine("Четверть № 4");
+// //     else if (x < 0 && y < 0)
+// //         System.Console.WriteLine("Четверть № 3");
+// //     else if (x < 0 && y > 0)
+// //         System.Console.WriteLine("Четверть № 2 ");
+// //     else
+// //         System.Console.WriteLine("Точка лежит на координатной оси");
+// // }
 
 
 
-void CheckKoord(int x, int y )
+// int CheckKoord2(int x, int y);
+// {
+//     int result = 0;
+//     if (x > 0 && y > 0)
+//         result = 1;
+//     else if (x > 0 && y < 0)
+//         result = 4;
+//      else if (x < 0 && y < 0)
+//         result = 3;
+//      else if (x < 0 && y > 0)
+//         result = 2;
+//       return result ;
+    
+// }
+
+
+// Console.WriteLine("Введите координату X");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Y");
+// int y = Convert.ToInt32(Console.ReadLine());
+// CheckKoord(x, y);
+// System.Console.WriteLine(CheckKoord2(x,y));
+
+
+// Задача 17 Напишите программу которая принимает на вход координаты точки (X и Y),
+// причем х не= 0 и у не=0 и выдает номер четверти плоскости в которой находится эта точка  
+
+
+
+ void CheckKoord(int x, int y)
 {
-   if (x > 0 && y > 0)
-       System.Console.WriteLine("Четверть № 1");
-     else if (x > 0 && y < 0)
-     System.Console.WriteLine("Четверть № 4");
-     else if (x < 0 && y < 0)
-     System.Console.WriteLine("Четверть № 3");
-     else if (x < 0 && y > 0)
-     System.Console.WriteLine("Четверть № 2 ");
-     else 
-     System.Console.WriteLine("Точка лежит на координатной оси");    
+    if (x > 0 && y > 0)
+        Console.WriteLine("Это четверть 1");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("Это четверть 4");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("Это четверть 3");
+    else if (x > 0 && y > 0)
+        Console.WriteLine("Это четверть 2");
+    else
+        Console.WriteLine("Эта точка лежит на координатной оси");
 }
+
+int CheckKoord2(int x, int y)
+{
+    int result = 0;
+        if (x > 0 && y > 0)
+        result = 1;
+    else if (x > 0 && y < 0)
+        result = 4;
+    else if (x < 0 && y < 0)
+        result = 3;
+    else if (x > 0 && y > 0)
+        result = 2;
+    return result;
+}
+
+Console.WriteLine("Ведите координаты X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите координаты Y");
+int y = Convert.ToInt32(Console.ReadLine());
+CheckKoord(x, y);
+Console.WriteLine($" Это четверть {CheckKoord2(x, y)} ");
---
+++ Examplealllekcii/Program.cs
@@ -0,0 +1,42 @@
+﻿// // метод. ничего не принимает и не возвращает
+// void Method1() 
+// {
+//      Console.WriteLine("Автор ...");
+// // }
+// Method1();
+
+// вид 2
+// Ничего не возвращает.Принимает аргументы
+// void Method2(string msq)
+// {
+//     Console.WriteLine(msq);
+// }
+// Method2("Текст сообщения");
+
+// void Method21(string msq, int count)
+// {
+//    int i = 0;   
+//    while(i< count)
+//    {
+//          Console.WriteLine(msq);
+//          i++;
+//    }
+// } 
+// // Method21("Текст" ,4);
+// Method21(count: 4, msq: "НОВЫЙ ТЕКСТ");
+
+// метод Возвращаютно ничего не принимают
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+    
+// }
+// int year = Method3();
+// Console.WriteLine(year);
+
+// // Методы 4 Принимают и возвроащают
+// string Method4(int count, string c)
+// {
+     
+// }
