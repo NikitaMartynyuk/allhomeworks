5bedca8e90cefc0a1d4bc8d101f2f8132ee9835d
Nikita Martynyuk <mnekita@yandex.ru>
2023-05-28 13:57:05 +0500

    Сделал задачу 34

---
+++ Homework4_29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] arr1 = {1, 2, 5, 7, 19};
+int[] arr2 = {6, 1, 33};
+
+Console.Write("[");
+for (int i = 0; i < arr1.Length; i++)
+{
+    Console.Write(arr1[i]);
+    if (i < arr1.Length - 1)
+    {
+        Console.Write(", ");
+    }
+}
+Console.WriteLine("]");
+
+Console.Write("[");
+for (int i = 0; i < arr2.Length; i++)
+{
+    Console.Write(arr2[i]);
+    if (i < arr2.Length - 1)
+    {
+        Console.Write(", ");
+    }
+}
+Console.WriteLine("]");
