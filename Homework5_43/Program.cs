ca1eae9ea3c8cc8cea5c9ff976781e29f73037cb
Nikita Martynyuk <mnekita@yandex.ru>
2023-05-31 21:09:24 +0500

    фибаначчи

--- Homework5_43/Program.cs
+++ Homework5_43/Program.cs
@@ -6,3 +6,18 @@
 // // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine("Точка пересечения: (" + x + ", " + y + ")");
+Console.ReadLine();
+
